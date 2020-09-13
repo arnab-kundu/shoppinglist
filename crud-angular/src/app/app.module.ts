9b6564dbf65646dbbfd7f4bc71f243a76cee6eb6
ARNAB <arnab.kundu1990@gmail.com>
2020-09-13 13:49:21 +0530

    Angular 1. Added Home, product, product-detail, page-not-found component

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -2,26 +2,39 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from "@angular/router";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ShoppingItemComponent} from './shopping-item/shopping-item.component';
-import { ProductComponent } from './product/product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {ProductComponent} from './product/product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'product-detail', component: ProductDetailComponent},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: 'crud', component: ShoppingItemComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingItemComponent,
+    HomeComponent,
     ProductComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    PageNotFoundComponent,
+    ShoppingItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
