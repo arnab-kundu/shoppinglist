aad5bea267696d0f1bfbddfff5250c91b52795e2
ARNAB <arnab.kundu1990@gmail.com>
2020-09-13 11:28:57 +0530

    1. Added Html, css, img 2. Added product, product-detail component

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -6,11 +6,16 @@ import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ShoppingItemComponent} from './shopping-item/shopping-item.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingItemComponent
+    ShoppingItemComponent,
+    ProductComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
