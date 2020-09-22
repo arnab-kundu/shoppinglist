8616a0a75fbc55f31865033e88f639ca3ef5af21
ARNAB <arnab.kundu1990@gmail.com>
2020-09-22 14:17:04 +0530

    NodeJs 1. Table created users, recently_viewed 2. RecentlyViewed create and select query implemented

--- crud-backend/app.js
+++ crud-backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
 var itemRouter = require('./routes/item');
 var productRouter = require('./routes/product');
+var recentlyViewedRouter = require('./routes/recently_viewd');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/api/item', itemRouter);
 app.use('/api/product', productRouter);
+app.use('/api/recently_viewed', recentlyViewedRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
