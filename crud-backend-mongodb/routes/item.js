692b8a9c50f90a8446a59d2b93f099b18a5f37a2
ARNAB <arnab.kundu1990@gmail.com>
2020-10-23 08:49:52 +0530

    Insert item to mongodb in POST method

--- crud-backend-mongodb/routes/item.js
+++ crud-backend-mongodb/routes/item.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 const Item = require('../model/shoppingItem');
 
+
+/**
+ * Add new Item.
+ * TYPE POST
+ * http://localhost:3000/api/item/add_item
+ * { id: 0, itemName: 'Mobile', itemQuantity: 1, itemBrought: false }
+ */
 router.post('/add_item', (req, res, next) => {
-    res.send("add_item");
+    console.log(req.url);
+    let newShoppingItem = new Item({
+        itemName: req.body.itemName,
+        itemQuantity: req.body.itemQuantity,
+        itemBrought: req.body.itemBrought
+    });
+    newShoppingItem.save((err, item) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({msg: "inserted"});
+        }
+    });
 });
 
 router.get('/items', (req, res, next) => {
