0c599937132ea76dcafb3acf338060583b2c15a6
ARNAB <arnab.kundu1990@gmail.com>
2020-11-01 21:58:45 +0530

    1. getAllUser API 2. recently_viewed product add & get API

--- crud-backend-mongodb/app.js
+++ crud-backend-mongodb/app.js
@@ -60,7 +60,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', usersRouter);
 app.use('/api/item', itemRouter);
 app.use('/api/product', productRouter);
 app.use('/api/recently_viewed', recentlyViewedRouter);
